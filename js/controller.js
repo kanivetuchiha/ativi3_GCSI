7763eaada0abafa4d5c66184b49bb5cc5a703a25
kanivetuchiha <erickazambuja.ms@gmail.com>
2025-10-02 09:36:27 -0300

    Revert "salvamento em branco"
    This reverts commit 563cea364a5bca637ae3e4d52e06b6195f940c0a.

--- js/controller.js
+++ js/controller.js
@@ -1,48 +1,17 @@
-import { readFileSync, writeFileSync, existsSync, renameSync } from "fs"
-import { dirname } from "path"
+import { readFileSync, writeFileSync } from "fs" 
+
+const carregardados = function(){ 
+    const dados = JSON.parse(readFileSync("dados.json")) 
+    return dados 
+} 
 
 
-const carregardados = function (filepath = "dados.json") {
-    try {
-        if (!existsSync(filepath)) {
-
-            return {}
-        }
-        const verificaSeNull = readFileSync(filepath, "utf8")
-
-        if (!verificaSeNull.trim()) return {}
-        const dados = JSON.parse(verificaSeNull)
-        return dados
-    } catch (err) {
-
-        throw new Error(`Falha ao carregar '${filepath}': ${err.message}`)
-    }
+const salvarDados = function(dados){
+    
+    writeFileSync("dados.json", "")  
+    writeFileSync("dados.json", JSON.stringify(dados, null, 2))
 }
-
-
-const salvarDados = function (dados, filepath = "dados.json", { backup = false } = {}) {
-    try {
-
-        const content = JSON.stringify(dados, null, 2)
-        const tmpPath = `${filepath}.tmp`
-        const backupPath = `${filepath}.bak`
-
-        if (backup && existsSync(filepath)) {
-            writeFileSync(backupPath, readFileSync(filepath, "utf8"), "utf8")
-        }
-
-        writeFileSync(tmpPath, content, "utf8")
-        renameSync(tmpPath, filepath)
-        return true
-    } catch (err) {
-
-        try {
-            if (existsSync(`${filepath}.tmp`)) {
-                writeFileSync(`${filepath}.tmp`, "", "utf8")
-}
-        } catch (_) { }
-        throw new Error(`Falha ao salvar '${filepath}': ${err.message}`)
-    }
-}
-
-export default {}
+    
+    
+    
+    export default{}
